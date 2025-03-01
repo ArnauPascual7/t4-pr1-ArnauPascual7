17cde5a851ecf43425549250fd2e3eadf86b9a71
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-03-01 16:59:15 +0100

    Updated Add Simulation Page Add Simulation Page is now FUNCTIONAL

--- EcoEnergyRazorPagesSolution/EcoEnergyRazorPages/Model/Simulation.cs
+++ EcoEnergyRazorPagesSolution/EcoEnergyRazorPages/Model/Simulation.cs
@@ -6,7 +6,7 @@
     }
     public class Simulation
     {
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
         public SystemType SysType { get; set; }
         public double ConfigPar { get; set; }
         public double Ratio { get; set; }
@@ -16,9 +16,65 @@
         public decimal TotalCost { get; set; }
         public decimal TotalPrice { get; set; }
 
+        public void SetSimulationCalculations()
+        {
+            switch(SysType)
+            {
+                case SystemType.SolarSystem:
+                    EnergyGen = ConfigPar * Ratio;
+                    break;
+                case SystemType.WindSystem:
+                    EnergyGen = Math.Pow(ConfigPar, 3) * Ratio;
+                    break;
+                case SystemType.HydroelectricSystem:
+                    EnergyGen = ConfigPar * 9.8 * Ratio;
+                    break;
+                default:
+                    EnergyGen = 0;
+                    break;
+            }
+            /*if (SysType is SystemType.SolarSystem)
+            {
+                EnergyGen = ConfigPar * Ratio;
+            }
+            else if (SysType is SystemType.WindSystem)
+            {
+                EnergyGen = Math.Pow(ConfigPar, 3) * Ratio;
+            }
+            else if (SysType is SystemType.HydroelectricSystem)
+            {
+                EnergyGen = ConfigPar * 9.8 * Ratio;
+            }
+            else
+            {
+                EnergyGen = 0;
+            }*/
+            TotalCost = KWHCost * (decimal)EnergyGen;
+            TotalPrice = KWHPrice * (decimal)EnergyGen;
+        }
+
+        public void SetSystemType(string system)
+        {
+            switch(system)
+            {
+                case "SolarSystem":
+                    SysType = SystemType.SolarSystem;
+                    break;
+                case "WindSystem":
+                    SysType = SystemType.WindSystem;
+                    break;
+                case "HydroelectricSystem":
+                    SysType = SystemType.HydroelectricSystem;
+                    break;
+                default:
+                    SysType = 0;
+                    break;
+            }
+        }
+
         public override string ToString()
         {
-            return $"{Date},{SysType},{ConfigPar},{Ratio},{EnergyGen},{KWHCost},{KWHPrice},{TotalCost},{TotalPrice}";
+            return $"{Date}|{SysType}|{ConfigPar}|{Ratio}|{EnergyGen}|{KWHCost}|{KWHPrice}|{TotalCost}|{TotalPrice}";
         }
     }
 }
--- EcoEnergyRazorPagesSolution/EcoEnergyRazorPages/Pages/AddSimulation.cshtml.cs
+++ EcoEnergyRazorPagesSolution/EcoEnergyRazorPages/Pages/AddSimulation.cshtml.cs
@@ -1,7 +1,11 @@
+using CsvHelper;
+using CsvHelper.Configuration;
 using EcoEnergyRazorPages.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
+using System.Globalization;
 using SysIO = System.IO;
 
 namespace EcoEnergyRazorPages.Pages
@@ -9,12 +13,16 @@ namespace EcoEnergyRazorPages.Pages
     public class AddSimulationModel : PageModel
     {
         public string MsgFileError;
+        [BindProperty]
         public Simulation NewSimulation { get; set; }
+        public List<SelectListItem> Systems { get; set; } =
+            Enum.GetValues(typeof(SystemType)).Cast<SystemType>().Select(v => new SelectListItem(v.ToString(), v.ToString())).ToList();
         public void OnGet()
         {
         }
-        public IActionResult OnPost()
+        public IActionResult OnPost(string systemtype)
         {
+            Debug.WriteLine("Simulation System Type --> " + systemtype);
             if (!ModelState.IsValid)
             {
                 return Page();
@@ -22,14 +30,26 @@ namespace EcoEnergyRazorPages.Pages
             string fileName = "simulacions_energia.csv";
             string filePath = @"ModelData\" + fileName;
             Debug.WriteLine("Simulations CSV File Path --> " + Path.GetFullPath(filePath));
-            /*if (SysIO.File.Exists(filePath))
+            NewSimulation.SetSystemType(systemtype);
+            NewSimulation.SetSimulationCalculations();
+            Debug.WriteLine("New Simulation --> " + NewSimulation.ToString());
+            if (SysIO.File.Exists(filePath))
             {
-                SysIO.File.AppendAllText(filePath, NewSimulation.ToString() + Environment.NewLine);
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HasHeaderRecord = false
+                };
+                using var stream = SysIO.File.Open(filePath, FileMode.Append);
+                using var writer = new StreamWriter(stream);
+                using var csvWriter = new CsvWriter(writer, config);
+                csvWriter.WriteRecord(NewSimulation);
             }
             else
             {
                 MsgFileError = "Error de càrrega de dades";
-            }*/
+                return Page();
+            }
+            SysIO.File.AppendAllText(filePath, Environment.NewLine);
             return RedirectToPage("Simulations");
         }
     }
