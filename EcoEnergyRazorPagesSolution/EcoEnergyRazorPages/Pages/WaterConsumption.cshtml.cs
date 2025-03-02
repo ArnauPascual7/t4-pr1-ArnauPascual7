b32e78b0ece5ed969909a29c1a69fc6b64e4b641
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-03-02 15:40:28 +0100

    Updated Water Consumption Page Added Statics for the municipalities with more water consumption and tryen to do other statics FAILED

--- EcoEnergyRazorPagesSolution/EcoEnergyRazorPages/Pages/WaterConsumption.cshtml.cs
+++ EcoEnergyRazorPagesSolution/EcoEnergyRazorPages/Pages/WaterConsumption.cshtml.cs
@@ -1,3 +1,5 @@
+using System.Collections.Immutable;
+using System.Diagnostics;
 using EcoEnergyRazorPages.Model;
 using EcoEnergyRazorPages.Tools;
 using Microsoft.AspNetCore.Mvc;
@@ -8,8 +10,13 @@ namespace EcoEnergyRazorPages.Pages
 {
     public class WaterConsumptionModel : PageModel
     {
+        public const int NumListMunicipalitiesWithMoreWater = 10;
+        public const int SusDigitsWaterConsumption = 3;
         public string MsgFileError;
         public List<WaterConsumption> WaterConsumptions { get; set; } = new List<WaterConsumption>();
+        public List<WaterConsumption> MunicipalitiesWithMoreWater { get; set; } = new List<WaterConsumption>();
+        /*public List<WaterConsumption> AverageWaterConsumptionByRegion { get; set; } = new List<WaterConsumption>();
+        public List<WaterConsumption> SusWaterConsumption { get; set; } = new List<WaterConsumption>();*/
         public void OnGet()
         {
             string fileName = "consum_aigua_cat_per_comarques.csv";
@@ -17,11 +24,45 @@ namespace EcoEnergyRazorPages.Pages
             if (SysIO.File.Exists(filePath))
             {
                 WaterConsumptions = FilesHelper.ReadCsv<WaterConsumption>(filePath);
+                MunicipalitiesWithMoreWater = CheckWaterConsumptioMostRecentYear(WaterConsumptions);
+                MunicipalitiesWithMoreWater.Sort(new WaterConsumptionComparer().HouseholdConsumptionPerCapitaCompare);
+                MunicipalitiesWithMoreWater.Reverse();
+                MunicipalitiesWithMoreWater.RemoveRange(NumListMunicipalitiesWithMoreWater - 1, MunicipalitiesWithMoreWater.Count - NumListMunicipalitiesWithMoreWater);
+                /*SusWaterConsumption = CheckSusWaterConsumption(WaterConsumptions, SusDigitsWaterConsumption);*/
             }
             else
             {
                 MsgFileError = "Error en la càrrega de dades";
             }
         }
+        public static List<WaterConsumption> CheckWaterConsumptioMostRecentYear(List<WaterConsumption> waterConsumptions)
+        {
+            List<WaterConsumption> MostRecentYearWaterConsumptions = new List<WaterConsumption>();
+            int year = 0;
+            foreach (WaterConsumption waterCons in waterConsumptions)
+            {
+                year = waterCons.Year > year ? waterCons.Year : year;
+            }
+            foreach (WaterConsumption waterCons in waterConsumptions)
+            {
+                if (waterCons.Year == year)
+                {
+                    MostRecentYearWaterConsumptions.Add(waterCons);
+                }
+            }
+            return MostRecentYearWaterConsumptions;
+        }
+        /*public static List<WaterConsumption> CheckSusWaterConsumption(List<WaterConsumption> waterConsumptions, int digits)
+        {
+            List<WaterConsumption> SusWaterConsumption = new List<WaterConsumption>();
+            foreach (WaterConsumption waterCons in waterConsumptions)
+            {
+                if (Convert.ToInt32(waterCons.HouseholdConsumptionPerCapita).ToString().Length > digits)
+                {
+                    SusWaterConsumption.Add(waterCons);
+                }
+            }
+            return SusWaterConsumption;
+        }*/
     }
 }
