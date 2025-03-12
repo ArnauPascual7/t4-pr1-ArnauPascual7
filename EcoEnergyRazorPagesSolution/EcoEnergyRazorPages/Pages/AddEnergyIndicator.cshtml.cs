0c2f78efd42734b4de6215a2d3602bbf2cb9e4e1
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-03-12 16:18:50 +0100

    Updated Add Energy Indicator Page With New Statement

--- EcoEnergyRazorPagesSolution/EcoEnergyRazorPages/Pages/AddEnergyIndicator.cshtml.cs
+++ EcoEnergyRazorPagesSolution/EcoEnergyRazorPages/Pages/AddEnergyIndicator.cshtml.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Diagnostics;
 using System.Text.Json;
 using EcoEnergyRazorPages.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Win32;
 using SysIO = System.IO;
 
 namespace EcoEnergyRazorPages.Pages
@@ -14,30 +16,51 @@ namespace EcoEnergyRazorPages.Pages
         public EnergyIndicator NewEnergyIndicator { get; set; }
         public void OnGet()
         {
+            string readFileName = "indicadors_energetics_defaultvalues.json";
+            string readFilePath = @"ModelData\" + readFileName;
+
+            if (SysIO.File.Exists(readFilePath))
+            {
+                string defaultJson = SysIO.File.ReadAllText(readFilePath);
+                EnergyIndicator? deserializedJson = JsonSerializer.Deserialize<EnergyIndicator>(defaultJson);
+
+                if (deserializedJson != null)
+                {
+                    NewEnergyIndicator = deserializedJson;
+                }
+                else
+                {
+                    MsgFileError = "Error de càrrega de dades";
+                }
+            }
+            else
+            {
+                MsgFileError = "Error de càrrega de dades";
+            }
         }
         public IActionResult OnPost()
         {
-            if (!ModelState.IsValid)
+            string writeFileName = "indicadors_energetics_cat.json";
+            string writeFilePath = @"ModelData\" + writeFileName;
+
+            if (SysIO.File.Exists(writeFilePath))
             {
-                return Page();
-            }
-            string fileName = "indicadors_energetics_cat.json";
-            string filePath = @"ModelData\" + fileName;
-            if (SysIO.File.Exists(filePath))
-            {
-                string existingJson = SysIO.File.ReadAllText(filePath);
+                string existingJson = SysIO.File.ReadAllText(writeFilePath);
                 List<EnergyIndicator> registers;
+
                 if (!string.IsNullOrEmpty(existingJson))
                 {
-                    registers = JsonSerializer.Deserialize<List<EnergyIndicator>>(existingJson);
+                    registers = JsonSerializer.Deserialize<List<EnergyIndicator>>(existingJson) ?? new List<EnergyIndicator>();
                 }
                 else
                 {
                     registers = new List<EnergyIndicator>();
                 }
                 registers.Add(NewEnergyIndicator);
+
                 string newJson = JsonSerializer.Serialize(registers);
-                SysIO.File.WriteAllText(filePath, newJson);
+
+                SysIO.File.WriteAllText(writeFilePath, newJson);
             }
             else
             {
